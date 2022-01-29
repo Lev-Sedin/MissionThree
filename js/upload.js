8f6244b24640df13c36ebe159788de48d58b6814
Lev-Sedin <79932008+Lev-Sedin@users.noreply.github.com>
2022-01-30 00:37:46 +0300

    Add files via upload

---
+++ js/upload.js
@@ -0,0 +1,42 @@
+const upFunc = () =>{
+   
+    const uploadFile = document.querySelector(".button__file");
+    const uploadBtn = document.querySelector(".donwoload__btn");
+    const uploadText = document.querySelector(".donwoload__text");
+
+    uploadBtn.addEventListener("click", function (){
+        uploadFile.click ();
+    })
+
+    uploadFile.addEventListener("change", function(){
+        if (uploadFile.value){
+           uploadText.innerText = uploadFile.value.match(/[\/\\]([\w\d\s\.\-(\)]+)$/)[1];
+         }else{
+            uploadText.innerText = "UPLOAD"
+        }
+            
+        })
+    
+}
+upFunc()
+
+const errorFuns =() =>{
+    const uploadFile = document.getElementById("button-error");
+    const uploadBtn = document.getElementById("donwoload-error");
+    const uploadText = document.getElementById("donwoload-text-error");
+
+    uploadBtn.addEventListener("click", function (){
+        uploadFile.click ();
+    })
+
+    uploadFile.addEventListener("change", function(){
+        if (uploadFile.value){
+            uploadText.innerText = uploadFile.value.match(/[\/\\]([\w\d\s\.\-(\)]+)$/)[1];
+        }else{
+            uploadText.innerText = "UPLOAD"
+        }
+            
+    })
+
+}
+errorFuns()
