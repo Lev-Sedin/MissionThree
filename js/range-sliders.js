10c1a89ecd9533d56bafc5dc412641684cd3839c
Lev-Sedin <79932008+Lev-Sedin@users.noreply.github.com>
2022-02-11 00:08:29 +0300

    Add files via upload

---
+++ js/range-sliders.js
@@ -0,0 +1,52 @@
+const rangeOne = document.querySelector("#slider__one")
+const rangeTwo = document.querySelector("#slider__two")
+const rangeThree = document.querySelector("#slider__three")
+const rangeFhour = document.querySelector("#slider__four")
+console.log(rangeOne)
+
+if (rangeOne){
+    noUiSlider.create(rangeOne, {
+        start: 40,
+        connect: 'lower',
+        range: {
+            'min': 0,
+            'max': 100
+        }
+    });
+}
+if(rangeTwo){
+    noUiSlider.create(rangeTwo, {
+        start: [20, 80],
+        connect: true,
+        range: {
+            'min': 0,
+            'max': 100
+        }
+    });
+}
+if(rangeThree){
+    noUiSlider.create(rangeThree, {
+        start: [80, 120],
+        step: 1,
+        connect: true,
+        tooltips: [true,  true],
+        range: {
+            'min': 0,
+            'max': 200
+        }
+    });
+}
+if(rangeFhour){
+    noUiSlider.create(rangeFhour, {
+        start: 120,
+        step: 1,
+       connect: 'lower',
+        tooltips: true,
+        range: {
+            'min': 0,
+            'max': 200
+        }
+    });
+}
+
+
