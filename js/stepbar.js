8d1cc3a7adc48ba83f8ed6e7269223d615370592
Lev-Sedin <79932008+Lev-Sedin@users.noreply.github.com>
2022-02-14 01:08:06 +0300

    Add files via upload

--- js/stepbar.js
+++ js/stepbar.js
@@ -1,3 +1,7 @@
+
+ 
+
+const stepHorizontal = (() =>{
 let els = document.getElementsByClassName('step');
 let steps = [];
 Array.prototype.forEach.call(els, (e) => {
@@ -6,20 +10,49 @@ Array.prototype.forEach.call(els, (e) => {
     progress(x.target.id);
   });
 });
+  function progress(stepNum) {
+    let p = stepNum * 32;
+    document.getElementsByClassName('step-bar__percent')[0].style.width = `${p}%`;
+    steps.forEach((e) => {
+      if (e.id === stepNum) {
+        e.classList.add('step-bar__selected');
+        e.classList.remove('step-bar__completed');
+      }
+      if (e.id < stepNum) {
+        e.classList.add('step-bar__completed');
+      }
+      if (e.id > stepNum) {
+        e.classList.remove('step-bar__selected', 'step-bar__completed');
+      }
+    });
+  }
+})
+stepHorizontal()
 
-function progress(stepNum) {
-  let p = stepNum * 32;
-  document.getElementsByClassName('step-bar__percent')[0].style.width = `${p}%`;
-  steps.forEach((e) => {
-    if (e.id === stepNum) {
-      e.classList.add('step-bar__selected');
-      e.classList.remove('step-bar__completed');
-    }
-    if (e.id < stepNum) {
-      e.classList.add('step-bar__completed');
-    }
-    if (e.id > stepNum) {
-      e.classList.remove('step-bar__selected', 'step-bar__completed');
-    }
+const stepVertical = (() =>{
+let els = document.getElementsByClassName('step');
+let steps = [];
+Array.prototype.forEach.call(els, (e) => {
+  steps.push(e);
+  e.addEventListener('click', (x) => {
+    progress(x.target.id);
   });
-}
+});
+  function progress(stepNum) {
+    let p = stepNum * 34;
+    document.getElementsByClassName('step-bar__percent__vertical')[0].style.height = `${p}%`;
+    steps.forEach((e) => {
+      if (e.id === stepNum) {
+        e.classList.add('step-bar__selected');
+        e.classList.remove('step-bar__completed');
+      }
+      if (e.id < stepNum) {
+        e.classList.add('step-bar__completed');
+      }
+      if (e.id > stepNum) {
+        e.classList.remove('step-bar__selected', 'step-bar__completed');
+      }
+    });
+  }
+})
+stepVertical()
